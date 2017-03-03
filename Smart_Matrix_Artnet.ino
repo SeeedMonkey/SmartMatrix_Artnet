cd7abd49beb702ba32f07b280507a19f8995281a
SeeedMonkey <bequiet_rules@web.de>
2017-03-03 18:48:55 +0100

    Update Smart_Matrix_Artnet.ino

--- Smart_Matrix_Artnet.ino
+++ Smart_Matrix_Artnet.ino
@@ -3,6 +3,28 @@
 
 //ARTNET.h : This example may be copied under the terms of the MIT license, see the LICENSE file for details
 //ADAFRUIT_SLEEPYDOG: This library was used under the terms of the MIT licence
+/*
+ * SmartMatrix Library:
+ *
+ * Copyright (c) 2015 Louis Beaudoin (Pixelmatix)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 #include <Wire.h>
 #include <SmartMatrix3.h>
 #include <FastLED.h>
